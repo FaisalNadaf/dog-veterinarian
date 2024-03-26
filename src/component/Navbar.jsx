44ac41f62641f46f1fe64f530fdf0c235578b8d6
faisal <faisalfnadaf@gmail.com>
2024-03-26 22:48:12 +0530

    project setUp

---
+++ src/component/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Navbar = () => {
+  return <div className="h-24 w-full bg-[#EAAE00] m-0 p-0">
+    <logo><img src="" alt="" /></logo>
+  </div>;
+};
+
+export default Navbar;
